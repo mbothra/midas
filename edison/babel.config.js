1a69a502b3877267d47bd69f1af1940724a01044
Amit Vishnoi <amit.vishnoi@finmechanics.com>
2021-04-08 00:19:05 +0530

    @removing babel dependencies

--- edison/babel.config.js
+++ edison/babel.config.js
@@ -5,8 +5,7 @@ module.exports = function(api) {
     presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'] ,
-        plugins: ['react-native-reanimated/plugin'],
+        plugins: ['react-native-paper/babel']
       },
     },
   };
